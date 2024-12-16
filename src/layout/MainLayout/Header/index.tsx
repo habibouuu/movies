ffb3956c6ef568e320cde088524c73b083f03bb6
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2024-12-16 12:51:52 -0500

    mess

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -47,7 +47,7 @@ const Header = () => {
   return (
     <>
       {/* logo & toggler button */}
-      <Box sx={{ width: downMD ? 'auto' : 228, display: 'flex' }}>
+      <Box sx={{ width: downMD ? 'auto' : 230, display: 'flex', display:'flex', justifyContent:'center', alignItems:'center' }}>
         <Box component="span" sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1 }}>
           <LogoSection />
         </Box>
