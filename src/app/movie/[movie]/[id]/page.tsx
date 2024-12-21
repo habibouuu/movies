f171bd6782d3bff9e9f89f5f83af612364a75a6a
Habib <79129877+Hackerbibou@users.noreply.github.com>
2024-12-21 11:52:07 -0500

    Update page.tsx

--- src/app/movie/[movie]/[id]/page.tsx
+++ src/app/movie/[movie]/[id]/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
     console.log(`https://vidsrc.to/embed/movie/${params.id}`)
     
   return (
-    <Container sx={{height:{xs:'460px', md:'500px', lg:'630px', mt:2}, display:'flex', flexDirection:'column'}}>
+    <Container sx={{height:{xs:'400px', md:'500px', lg:'630px', mt:2}, display:'flex', flexDirection:'column'}}>
         <iframe sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" width="100%" height="100%" src={`https://vidsrc.to/embed/movie/${params.id}`} title={params.movie+''} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       <Box sx={{p:3}}/>
       <Typography variant='h1'>{params.movie && params.movie.split('%20').join(' ').split('%3').join(': ')}</Typography>
