fe65f49241d73d2782e1266379269d71515b0408
Habib <79129877+habibouuu@users.noreply.github.com>
2025-02-26 22:53:08 -0500

    check

--- src/app/tvshow/[show]/[id]/page.tsx
+++ src/app/tvshow/[show]/[id]/page.tsx
@@ -10,7 +10,8 @@ export default function page() {
     <Container sx={{height:{xs:'400px', md:'500px', lg:'630px', mt:2}, display:'flex', flexDirection:'column'}}>
         <iframe sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" width="100%" height="100%" src={`https://vidsrc.to/embed/tv/${params.id}`} title={params.show+''} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       <Box sx={{p:3}}/>
-      <Typography variant='h1'>{params.show && params.show.split('%20').join(' ').split('%3').join(': ')}</Typography>
+      <Typography variant='h1'>{params.show && params.show.split('%20').join(' ').split('%3').join(': ').split('%26').join('&')}</Typography>
+      
     </Container>
   )
 }
